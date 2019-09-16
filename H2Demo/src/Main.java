15f6a6c2aa44c04299ce481c8af881257089b899
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2019-09-16 19:27:54 -0400

    Add files via upload

---
+++ H2Demo/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+class Main {
+    public static void main(String[] args) throws IOException, SQLException {
+        ProductManager pm = new ProductManager();
+        pm.selectAll();
+
+        // Finally let's insert some data
+        // Will use stringBuilder or similar in video to build/map this
+        // Main point for both: USE PLACEHOLDERS
+        String insertQuery = "INSERT INTO employee " +
+                "(uid, name)" +
+                " VALUES (?, ?)";
+        String[] itemp = {"3889", "Bob King"};
+
+        pm.insertProd(insertQuery, itemp);
+        pm.selectAll();
+
+
+        // And close our connection at end
+        pm.closeCon();
+    }
+}
